0cbac9268ba27f5c65f8f832a18087e63f3f3809
Irving Uriarte Navarrete <61202443+IrvingSSInmegen@users.noreply.github.com>
2020-08-03 11:04:45 -0500

    Update Proyecto.R

--- Proyecto.R
+++ Proyecto.R
@@ -77,36 +77,79 @@ COAD_StIIIandIV_biom_STN <- dplyr::semi_join(x = kraken_TCGA_Voom_Novirus,
                                            y = COAD_STN_StIIIandIV,
                                            by = c("...1" = "...1")) #9 samples
 
-
-
 # Matrices de conteo de abundancia
 
-COAD_StIandII_biom_PT_1 <- COAD_StIandII_biom_PT %>% select(-...1)
-Samples1 <- COAD_StIandII_biom_PT$...1
-AbMat_COAD_StIandII_biom_PT <- as.matrix(t(COAD_StIandII_biom_PT_1))  # StageIandII Primary Tumor
-colnames(AbMat_COAD_StIandII_biom_PT) <- Samples1
+# StageIandII Primary Tumor
+COAD_StIandII_biom_PT_trans <- t(COAD_StIandII_biom_PT) %>% row_to_names(row_number = 1)
 
-COAD_StIandII_biom_BDN_1 <- COAD_StIandII_biom_BDN %>% select(-...1)
-Samples2 <- COAD_StIandII_biom_BDN$...1
-AbMat_COAD_StIandII_biom_BDN <- as.matrix(t(COAD_StIandII_biom_BDN_1)) # StageIandII Blood Derived Normal
-colnames(AbMat_COAD_StIandII_biom_BDN) <- Samples2
+# StageIandII Blood Derived Normal
+COAD_StIandII_biom_BDN_trans <- t(COAD_StIandII_biom_BDN) %>% row_to_names(row_number = 1)
 
-COAD_StIandII_biom_STN_1 <- COAD_StIandII_biom_STN %>% select(-...1)
-Samples3 <- COAD_StIandII_biom_STN$...1
-AbMat_COAD_StIandII_biom_STN <- as.matrix(t(COAD_StIandII_biom_STN_1)) # StageIandII Solid Tissue Normal
-colnames(AbMat_COAD_StIandII_biom_STN) <- Samples3
+# StageIandII Solid Tissue Normal
+COAD_StIandII_biom_STN_trans <- t(COAD_StIandII_biom_STN) %>% row_to_names(row_number = 1)
 
-COAD_StIIIandIV_biom_PT_1 <- COAD_StIIIandIV_biom_PT %>% select(-...1)
-Samples4 <- COAD_StIIIandIV_biom_PT$...1
-AbMat_COAD_StIIIandIV_biom_PT <- as.matrix(t(COAD_StIIIandIV_biom_PT_1)) # StageIIIandIV Primary Tumor
-colnames(AbMat_COAD_StIIIandIV_biom_PT) <- Samples4
+# StageIIIandIV Primary Tumor
+COAD_StIIIandIV_biom_PT_trans <- t(COAD_StIIIandIV_biom_PT) %>% row_to_names(row_number = 1)
 
-COAD_StIIIandIV_biom_BDN_1 <- COAD_StIIIandIV_biom_BDN %>% select(-...1)
-Samples5 <- COAD_StIIIandIV_biom_BDN$...1
-AbMat_COAD_StIIIandIV_biom_BDN <- as.matrix(t(COAD_StIIIandIV_biom_BDN_1)) # StageIIIandIV Blood Derived Normal
-colnames(COAD_StIIIandIV_biom_BDN_1) <- Samples5
+# StageIIIandIV Blood Derived Normal
+COAD_StIIIandIV_biom_BDN_trans <- t(COAD_StIIIandIV_biom_BDN) %>% row_to_names(row_number = 1)
 
-COAD_StIIIandIV_biom_STN_1 <- COAD_StIIIandIV_biom_STN %>% select(-...1)
-Samples6 <- COAD_StIIIandIV_biom_STN$...1
-AbMat_COAD_StIIIandIV_biom_STN <- as.matrix(t(COAD_StIIIandIV_biom_STN_1)) # StageIIIandIV Solid Tissue Normal
-colnames(COAD_StIIIandIV_biom_STN_1) <- Samples6
+# StageIIIandIV Solid Tissue Normal
+COAD_StIIIandIV_biom_STN_trans <- t(COAD_StIIIandIV_biom_STN) %>% row_to_names(row_number = 1)
+
+##########################
+#######            #######
+######   GRÁFICAS   ################################
+#######            #######
+##########################
+
+# Idea sacar la media de las columnas nos dira cual es mas abundante??
+# Si es así entonces acomodar en max- min
+# presentar un gráfica de pastel para ver cierto porcentaje
+# Vero como hacer los geom_violin()
+#  Encontrar una manera de solo gráficas los mas importantes
+#  o en su defecto gráficar el del interes propio.
+
+library(ggplot2)
+
+#  Como las muestras de tejido sólido tienen mas de 100 muestras
+#   son las que se analizarán
+
+# Data
+# Matrices de conteo de abundancia 
+
+# COAD_StIandII_biom_PT <- Matriz.primary tumor
+
+#COAD_StIIIandIV_biom_PT <- Matriz.primary tumor 
+
+
+Media_StIandII_PT <- COAD_StIandII_biom_PT %>% select(-...1) %>%
+                      summarise_all(mean) %>%
+                      t() %>%
+                      as_tibble(rownames = "Bac_Arch")%>%
+                      arrange(desc(V1))
+
+Media_StIIIandIV_PT <- COAD_StIIIandIV_biom_PT %>% select(-...1) %>%
+                        summarise_all(mean) %>%
+                        t() %>%
+                        as_tibble(rownames = "Bac_Arch") %>%
+                        arrange(desc(V1))
+
+StIandII_PT_vplot <- COAD_StIandII_biom_PT %>% select(...1,k__Bacteria.p__Proteobacteria.c__Gammaproteobacteria.o__Oceanospirillales.f__Halomonadaceae.g__Cobetia)
+
+StIandII_PT_vplot_pivot %>% StIandII_PT_vplot %>% tidyr::pivot_longer(cols = -"...1",
+                                                                     names_to = "Bacteria",
+                                                                     values_to = "Valores") %>%
+                                                 select(-...1) 
+
+
+
+StIandII_PT_vplot %>% tidyr::pivot_longer(cols = -"...1",
+                                          names_to = "Bacteria",
+                                          values_to = "Valores")%>%
+                              ggplot(mapping = aes(x = "Valores",
+                                                   y = "Bacteria",
+                                                   fill = as.factor(Bacteria))) +
+                              geom_boxplot() + 
+                              facet_wrap(~as.factor(Bacteria),scales = "free")
+
